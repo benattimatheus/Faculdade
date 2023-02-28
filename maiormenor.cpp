9e1be91a81ce0193f4189053d14643886851a099
Matheus Benatti <matheus.cwb2@gmail.com>
2023-02-28 16:07:02 -0300

    Update maiormenor.cpp

--- maiormenor.cpp
+++ maiormenor.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    //Declaração de variáveis
+    
     int vet[10], i, maior, menor;
-    //Loop para que o usuário digite um número para cada valor do vetor. O i é responsável por aumentar a posição do vetor. Esse valor é salvo dentro do vetor com o scanf.
+   
     for (i = 0; i<10; i++){
         printf("\nFale o %d° valor:", i+1);
         scanf("%i", &vet[i]);
     }
-    //Maior e menor são declarados como a primeira variável. O loop verifica se o valor é maior do que a primeira váriavel ou menor. Se satisfazer alguma dessas condições, ela é substituída por esse novo valor
+    
     maior = vet[0];
     menor = vet[0];
     for (i=0; i<10; i++){
